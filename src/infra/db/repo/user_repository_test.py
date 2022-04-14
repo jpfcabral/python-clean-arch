75069e9bd0e486010a13fcb0e1f3606c7b292f5a
jpfcabral <jpfcabral@gmail.com>
2022-04-14 16:24:43 -0300

    add: user selection tests

--- src/infra/db/repo/user_repository_test.py
+++ src/infra/db/repo/user_repository_test.py
@@ -22,3 +22,29 @@ def test_insert_user():
     assert user.password == query_user.password
 
     user_repository.delete_user_by_id(user_id=user.id)
+
+
+def test_select_user():
+    db_connection_handler.create_db_and_tables()
+
+    name = fake.name()
+    email = fake.email()
+    password = fake.word()
+
+    user = user_repository.insert_user(name, email, password)
+
+    query_user1 = user_repository.get_user_by_id(user.id)
+    query_user2 = user_repository.get_users_by_email(user.email)
+    query_user3 = user_repository.get_users_by_name(user.name)
+
+    assert user == query_user1
+    assert user in query_user2
+    assert user in query_user3
+
+    query_user4 = user_repository.select_user(id=user.id)
+    query_user5 = user_repository.select_user(email=user.email)
+    query_user6 = user_repository.select_user(name=user.name)
+
+    assert user in query_user4
+    assert user in query_user5
+    assert user in query_user6
